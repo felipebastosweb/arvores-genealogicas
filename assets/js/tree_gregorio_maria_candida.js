c38713a73349aca10807c1f8efc39e624d9b4b3c
felipe bastos <felipebastosweb@gmail.com>
2020-12-14 18:54:32 -0300

    Add files via upload

---
+++ assets/js/tree_gregorio_maria_candida.js
@@ -0,0 +1,215 @@
+var tree_gregorio_maria_candida = {
+    
+      100: {
+        102: '',
+        103: {
+          // Filhos de Jose Albino
+          200: '',
+          201: '',
+          202: ''
+        },
+        104: '',
+        105: '',
+        106: '',
+        107: '',
+        108: {
+          // Filhos de Marina
+          300: '',
+          301: '',
+          302: '',
+        },
+        109: {
+          // Filhos de Sergina
+          400: '',
+          401: '',
+          402: '',
+        },
+        1: {
+            47: {
+              48: {
+                56: '',
+                57: '',
+              },
+              49: {
+                58: '',
+                59: ''
+              },
+              50: {
+                60: '',
+              },
+              51: {
+                61: '',
+                62: ''
+              },
+              52: '',
+              53: {
+                63: ''
+              },
+              54: '',
+              55: ''
+            },
+            2: {
+              19: {
+                40: '',
+                42: ''
+              },
+              20: {
+                41: ''
+              },
+              21: '',
+              22: ''
+            },
+            3: {
+              14: {
+                16: ''
+              },
+              15: '',
+            },
+            4: {
+              12: '',
+              13: {
+                43: ''
+              }
+            },
+            5: {
+              17: '',
+              18: {
+                45: ''
+              },
+              26: ''
+            },
+            6: {
+              23: '',
+              24: ''
+            },
+            7: {
+              25: ''
+            },
+            8: {
+              27: {
+                30: '',
+                31: ''
+              },
+              28: '',
+              29: ''
+            },
+            9: {
+              34: ''
+            },
+            10: {
+              35: {
+                46: '',
+              },
+              36: {
+                64: '',
+              },
+              37: '',
+              38: '',
+              39: ''
+            },
+            11: {
+              32: '',
+              33: ''
+            }
+        },
+    },
+};
+
+let treeParams_tree_gregorio_maria_candida = {
+    1: {trad: 'Antonio Herculano de Oliveira Bastos e Eunice da Cruz Bastos'},
+    2: {trad: 'Joel da Cruz Bastos (Inêz Andrade/Ana Valéria)'},
+    3: {trad: 'Elienai da Cruz Bastos (Leopoldo Santana Moraes)'},
+    4: {trad: 'Antonio da Cruz Bastos (Romilda Lima)'},
+    5: {trad: 'Josafa da Cruz Bastos e Cleilda Costa'},
+    6: {trad: 'Adonias da Cruz Bastos e Ivânia Silva'},
+    7: {trad: 'Miriã da Cruz Bastos'},
+    8: {trad: 'Marta da Cruz Bastos e Samuel Dantas'},
+    9: {trad: 'Adauto da Cruz Bastos e Jhancy Vidal'},
+    10: {trad: 'Jesse da Cruz Bastos (...)'},
+    11: {trad: 'Davi da Cruz Bastos e Léia Ramos'},
+    12: {trad: 'Rafael Lima Bastos'},
+    13: {trad: 'Erica Lima Bastos e Jeremias Almeida'},
+    14: {trad: 'Felipe Augusto Bastos Moraes e Joelma Bastos'},
+    15: {trad: 'Elienai Elisia Bastos Moraes'},
+    16: {trad: 'Augusto Bastos Moraes'},
+    17: {trad: 'Jackson Costa Bastos'},
+    18: {trad: 'Jessica Costa Bastos'},
+    19: {trad: 'Carla Andrade Bastos'},
+    20: {trad: 'Fabio Andrade Bastos'},
+    21: {trad: 'Kleyton Bastos'},
+    22: {trad: 'Ana Vitoria Bastos'},
+    23: {trad: 'Thiago Silva Bastos'},
+    24: {trad: 'Bianca Silva Bastos'},
+    25: {trad: 'Laine Bastos Franca'},
+    26: {trad: 'Nicholas Costa Bastos'},
+    27: {trad: 'Suelem Bastos Dantas'},
+    28: {trad: 'Samuel Bastos Dantas'},
+    29: {trad: 'Rafael Bastos Dantas'},
+    30: {trad: 'Pedro Dantas'},
+    31: {trad: 'Melissa Dantas'},
+    32: {trad: 'Dara Ramos Bastos'},
+    33: {trad: 'Davi Ramos Bastos'},
+    34: {trad: 'Jhoany Vidal Bastos'},
+    35: {trad: 'Paula Cerqueira'},
+    36: {trad: 'Jessica ...'},
+    37: {trad: 'Raiana Medrado'},
+    38: {trad: 'Rafaela Medrado'},
+    39: {trad: 'Guilherme Medrado'},
+    40: {trad: 'Abraão Bastos Rebouças'},
+    41: {trad: 'Caetano Bastos'},
+    42: {trad: 'Bianca Bastos Rebouças'},
+    43: {trad: 'Miguel Bastos Almeida'},
+    45: {trad: 'Guilherme Bastos'},
+    46: {trad: 'Bernardo Cerqueira'},
+    47: {trad: 'Jairo Mendes da Cruz'},
+    48: {trad: 'Jueli da Cruz'},
+    49: {trad: 'Judiceia da Cruz'},
+    50: {trad: 'Jadson da Cruz'},
+    51: {trad: 'Geiziane da Cruz'},
+    52: {trad: 'Joari da Cruz'},
+    53: {trad: 'Jocinaldo da Cruz'},
+    54: {trad: 'Josilene da Cruz'},
+    55: {trad: 'Jailma da Cruz'},
+    56: {trad: 'Hozana Cruz'},
+    57: {trad: 'Thaynara Cruz'},
+    58: {trad: 'Melissa Cruz'},
+    59: {trad: 'Alice Cruz'},
+    60: {trad: 'Mateus Cruz'},
+    61: {trad: 'Luize Cruz'},
+    62: {trad: 'Mikaele Cruz'},
+    63: {trad: 'Joaquim Cruz'},
+    64: {trad: 'Miguel de Jéssica'},
+    100: {trad: 'Gregório de Oliveira Bastos e Maria Cândida'},
+    102: {trad: 'Ermírio Malaquias de Oliveira Bastos'},
+    103: {trad: 'José Albino de Oliveira Bastos'},
+    104: {trad: 'Beatriz de Oliveira Bastos'},
+    105: {trad: 'Betina de Oliveira Bastos'},
+    106: {trad: 'Jardelina de Oliveira Bastos'},
+    107: {trad: 'Francisco de Assis de Oliveira Bastos'},
+    108: {trad: 'Marina de Oliveira Bastos'},
+    109: {trad: 'Sergina de Oliveira Bastos'},
+    110: {trad: 'Maria Cândida de Oliveira Bastos (Mariá)'},
+    // Filhos de Jose Agripino??
+    200: {trad: 'Isaías Bastos'},
+    201: {trad: 'Paulo Bastos'},
+    202: {trad: 'Gerson de O. Bastos'},
+    // Filhos de Marina
+    300: {trad: 'Adriana Bastos'},
+    301: {trad: 'Paulo Dias'},
+    302: {trad: 'Sergio Dias'},
+    // Filhos de Sergina
+    400: {trad: 'Jose Roberto Bastos Leal'},
+    401: {trad: 'Reinaldo Bastos Leal'},
+    402: {trad: 'Luiz Rogério Leal'},
+    1000: {trad: 'Coronel Manoel Moreira Bastos'},
+    1001: {trad: 'Manoel Roque Moreira Bastos'}
+};
+
+treeMaker(tree_gregorio_maria_candida, {
+    id: 'id_tree_gregorio_maria_candida', card_click: function (element) {
+        console.log(element);
+    },
+    treeParams: treeParams_tree_gregorio_maria_candida,
+    'link_width': '4px',
+    'link_color': '#2199e8',
+});
